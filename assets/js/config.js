629e4b3043eac4a79d7b09a29f72537f54d014dc
Marcos Wang <wangfmarcos@gmail.com>
2023-10-31 12:48:36 -0700

    Added API key, working on implementation.

---
+++ assets/js/config.js
@@ -0,0 +1,3 @@
+const config={
+    APIKey: "4811a02389f8a80db14da74b61f8620f"
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
-
-$(function () {
   var now = dayjs();
   var nowEl = $('#currentDay');
   nowEl.text((now.format('dddd, MMMM D YYYY')));
-});
+
+  let searchEl = document.getElementById('user-input');
+  let searchButton = document.getElementById('search');
+ 
+  let inputsearch = function(event){
+    event.preventdefault();
+  }
+  let getWeather = function(input){
+    const myKey = config.APIKey;
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input + '&appid=' + myKey + '&units=imperial')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data){
+      if (data.message === "city not found" || data === "[]"){
+        alert("City could not be found");
+      } else{
+        //append city to list
+        
+        
+
+
+      }
+    })
+  }
